7ed0dd405ad747c9f559359a3fee6be9e86b6970
Nathan Hebert <hebertn87@gmail.com>
2019-04-18 16:44:38 -0600

    pushed up dbo tables to use for future

---
+++ HackerChatDB/MessageLogging.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[MessageLogging]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [timestamps] TIMESTAMP NULL, 
+    [actionID] NVARCHAR(MAX) NULL, 
+    [owner] NVARCHAR(MAX) NULL, 
+    [message] NVARCHAR(MAX) NULL,
+	UserID int FOREIGN KEY REFERENCES Users(Id)
+)
---
+++ HackerChatDB/Users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [uname] NVARCHAR(MAX) NOT NULL, 
+    [pword] NVARCHAR(MAX) NOT NULL
+)
