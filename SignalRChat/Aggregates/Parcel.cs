f649af0a78e15c35afa5d2c7efec16ba1edf7538
Ammon Riley <amm0theman@live.com>
2019-04-18 12:06:34 -0600

    added stuff forgot in last commit

---
+++ SignalRChat/Aggregates/Parcel.cs
@@ -0,0 +1,14 @@
+﻿using SignalRChat.Value_Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalRChat.Entities
+{
+    class Parcel
+    {
+        private IOwner Owner { get; }           //Creator/owner of the message
+        private TimeStamp TimeStamp { get; }    //Time of message creation
+        private Message message { get; }        //Message contained in parcel
+    }
+}
